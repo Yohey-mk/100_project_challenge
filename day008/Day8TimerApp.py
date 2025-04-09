0df8bda436f38e48a5b01c4d8cfc3c8b12c4aab6
Yohey-mk <youheiyamaguchi0327@gmail.com>
2025-04-09 21:32:52 +0900

    Modified countdown timer code

--- day008/Day8TimerApp.py
+++ day008/Day8TimerApp.py
@@ -1,12 +1,50 @@
 #Day8 Countdown Timer App
 
 import time
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Focus Timer")
+root.geometry("300x200")
+label = tk.Label(root, text="Welcome to the Focus Timer!")
+label.pack()
+root.mainloop()
+
+def focus_template():
+    try:
+        user_options = [10, 25, 50]
+        print(f"1. {user_options[0]}mins")
+        print(f"2. {user_options[1]}mins")
+        print(f"3. {user_options[2]}mins")
+        print(f"4. Enter focus time manually.")
+        print(f"5. Add focus time template.") #後で実装
+        print("Enter Q to quit")
+        user_choice = input("Choose focus time from the list above:")
+        if user_choice == "1":
+            print(f"Okay! Let's focus for {user_options[0]}mins!")
+            return int(user_options[0]) * 60
+        elif user_choice == "2":
+            print(f"Okay! Let's focus for {user_options[1]}mins!")
+            return int(user_options[1]) * 60
+        elif user_choice == "3":
+            print(f"Okay! Let's focus for {user_options[2]}mins!")
+            return int(user_options[2]) * 60
+        elif user_choice == "4":
+            focus_time_input = input("Enter the time you want to focus: ")
+            return int(focus_time_input) * 60
+        elif user_choice == "q":
+            exit()
+        else:
+            return
+    except ValueError:
+        print("Invalid input. Enter again.")
+        return focus_template()
 
 def user_input():
     time_select = int(input("Enter the minutes you want to focus: "))
     return time_select * 60
-
-for time_remaining in range(user_input(), 1, -1):
+ 
+for time_remaining in range(focus_template(), 0, -1):
     minutes_left, seconds_left = divmod(time_remaining, 60)
     print(f"{minutes_left:02}:{seconds_left:02}")
     time.sleep(1)
@@ -16,9 +54,12 @@ print("Timer finished!")
 
 #This is a test to see if I successfuly synced my local env and Github!
 #Notes
-#timerの表示が２→Finished!となるのはUX的に良くない
-#2-->1-->Finished!となったほうが自然
-#mm:ssの表示に変換する
+#✔timerの表示が２→Finished!となるのはUX的に良くない
+#✔2-->1-->Finished!となったほうが自然
+#✔mm:ssの表示に変換する
 #自分で時間を入力するか、テンプレートの時間を選択してタイマーを開始する
 #└Windowポップアップを出してマニュアル入力かテンプレ選択でタイマーを開始できるようにする
 #timerを途中でやめる機能をつける。特定のキー（ESCかQ）を押すか、GUI版ではキャンセルボタンを押すか
+
+#Learning Notes
+#for time_remaining in range(user_input(), 1, -1): --> ..., 0, -1)とした方が、0:00で終了となり自然
--- day009/Day9 Test(Github).py
+++ day009/Day9 Test(Github).py
@@ -1,3 +1,9 @@
 #Day9 Test(Github)
 
 #test
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Focus Timer")
+root.geometry("300x200")
+root.mainloop()
