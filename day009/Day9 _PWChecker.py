1354d46a653f94dd5a21a3d758a58eb93075e650
Yohey-mk <youheiyamaguchi0327@gmail.com>
2025-04-15 21:33:08 +0900

    Modified the code, adding pw gen function

--- day009/Day9 _PWChecker.py
+++ day009/Day9 _PWChecker.py
@@ -1,8 +1,9 @@
 #Day9 パスワード強度チェッカー
 import string
 import csv #csvにあるリストを使いPWの脆弱性をチェックする
+import random #password genに使う
 
-def load_pw_list(Most_common_passwords): #get_csv()とかを作成して、CSVのアップロード/読み込み機能をつける、または指定フォルダのCheck用PW保管用のCSVファイルを表示する
+def load_pw_list(Most_common_passwords): #get_csv_list()とかを作成して、CSVのアップロード/読み込み機能をつける、または指定フォルダのCheck用PW保管用のCSVファイルを表示する？
     with open("Most_common_passwords.csv", newline='') as csvfile:
         return {row[0] for row in csv.reader(csvfile)}
 
@@ -26,7 +27,13 @@ def check_password(password):
     else:
         return "Your password is strong."
     
-
+gen_length = int(input("Set a length of password generation: ")) #後に数字/アルファベット（大文字小文字）/記号オンリーなどの組み合わせを選べるようにする
+def generate_password(char_length=gen_length):
+    char_length = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(char_length) for _ in range(gen_length)) #gen pwにlower/upper caseのどちらも含める
+    
+gen_password = generate_password()
+print(f"Generated password: {gen_password}")
 password = input("Enter your password: ")
 result = check_password(password)
 print(result)
@@ -48,5 +55,6 @@ print(result)
 #most common passwordsのリストをcsvなどで作成し、それをインポートして、
 #ユーザーが入力したパスワードと一致しているかチェックする。もし一致した場合はAlertを出す
 #Password generation function
+#--> random, stringを使う
 #Passwordが連続して同じ英数字を使っていたら駄目とする --> How?
 
