d0552828d801bb9145bacd70f35d12bd91b05615
Yohey-mk <youheiyamaguchi0327@gmail.com>
2025-05-05 19:10:11 +0900

    added a basic component

--- day012/calendar_with_calculator.py
+++ day012/calendar_with_calculator.py
@@ -1,1 +1,36 @@
 #Day 12: 電卓付きカレンダーアプリ（CLI → GUI）
+
+###imports
+from datetime import datetime, timedelta
+
+###Helper / Background functions
+
+
+###App Logics
+def get_user_input():
+    pass
+
+def date_calculation():
+    pass
+
+def print_result():
+    pass
+
+###Main
+def main():
+    pass
+
+###UI Components
+
+
+###Run App
+if __name__ == '__main__':
+    main()
+
+
+
+
+#Leaning notes
+#🎯目標：
+#	•	日付入力から曜日を判定し、さらに簡単な日付計算（○日後や○日前）を行えるCLIアプリを作成。
+#	•	GUI版では、日付選択UIと結果表示エリアを備えたアプリに。
