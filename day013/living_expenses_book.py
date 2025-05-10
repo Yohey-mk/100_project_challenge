319d998c9feafbd0256d4572ab2516b978cbeada
Yohey-mk <youheiyamaguchi0327@gmail.com>
2025-05-10 21:57:11 +0900

    initial commitment

---
+++ day013/living_expenses_book.py
@@ -0,0 +1,17 @@
+#Day13: シンプル家計簿アプリ（CLI → GUI）
+# === Imports ===
+
+
+# === Helper Function ===
+
+
+# === App Logics ===
+
+
+# === UI Components ===
+
+
+# === UI Interface ===
+
+
+# === Run App ===
