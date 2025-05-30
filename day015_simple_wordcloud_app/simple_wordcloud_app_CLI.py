ad20100a65882e97f3249907ba8a785cf4df823e
Yohey-mk <youheiyamaguchi0327@gmail.com>
2025-05-30 21:29:54 +0900

    initial commitment, created a cli template

---
+++ day015_simple_wordcloud_app/simple_wordcloud_app_CLI.py
@@ -0,0 +1,25 @@
+# simple_wordcloud_app.py
+# CLI Ver.
+
+### === Imports===
+
+
+### === Helper Functions ===
+
+
+### === App Logics ===
+def main():
+    pass
+
+### === UI Components ===
+
+
+### === UI Interfaces ===
+
+
+### === Run App ===
+if __name__ == '__main__':
+    main()
+
+
+### === Notes ===
