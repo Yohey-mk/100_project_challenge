3ccf9c5733428859c999d0f96cc3a3043cc51a89
Yohey-mk <youheiyamaguchi0327@gmail.com>
2025-06-11 07:30:30 +0900

    changed folder name

---
+++ day016_TranslatorApp/translator_app.py
@@ -0,0 +1,18 @@
+# translator_app.py
+
+### === Imports ===
+from translate import Translator
+
+
+### === Helper Functions ===
+
+
+### === App Logics ===
+def main():
+    pass
+
+### === Main ===
+if __name__ == "__main__":
+    main()
+
+
