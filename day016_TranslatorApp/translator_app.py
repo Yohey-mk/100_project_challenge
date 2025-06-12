5becf7016b1c65448ccc191b9156141a88360ecf
Yohey-mk <youheiyamaguchi0327@gmail.com>
2025-06-13 05:56:13 +0900

    testing translate and argostranslate

--- day016_TranslatorApp/translator_app.py
+++ day016_TranslatorApp/translator_app.py
@@ -2,14 +2,16 @@
 
 ### === Imports ===
 from translate import Translator
-
+import sentencepiece as spm
 
 ### === Helper Functions ===
 
-
 ### === App Logics ===
 def main():
-    pass
+    translator = Translator(to_lang="ja")
+    translation = translator.translate("Good morning folks!")
+    print(translation)
+    print(spm.__version__)
 
 ### === Main ===
 if __name__ == "__main__":
