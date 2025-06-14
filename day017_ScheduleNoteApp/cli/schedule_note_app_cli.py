d081bc27090d9968753749f519704c01203876dd
Yohey-mk <youheiyamaguchi0327@gmail.com>
2025-06-14 17:47:17 +0900

    initial commit

---
+++ day017_ScheduleNoteApp/cli/schedule_note_app_cli.py
@@ -0,0 +1,17 @@
+# schedule_note_app_cli.py
+
+### === Imports ===
+
+
+### === Helper Functions ===
+
+
+### === App Logics ===
+def main():
+    pass
+
+### === Run App ===
+if __name__ == "__main__":
+    main()
+
+### === Notes ===
