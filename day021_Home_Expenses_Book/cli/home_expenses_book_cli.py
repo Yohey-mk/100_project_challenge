c01639f37b5edf714781f7d298b041951e866257
Yohey-mk <youheiyamaguchi0327@gmail.com>
2025-07-17 06:44:36 +0900

    initial setup

---
+++ day021_Home_Expenses_Book/cli/home_expenses_book_cli.py
@@ -0,0 +1,31 @@
+# home_expenses_book_cli.py
+
+### === Imports ===
+import pandas as pd
+import csv
+
+
+### === Helper Functions ===
+# handle_csvみたいなモジュール化してそこにまとめてもいいかも
+def load_csv():
+    pass
+
+def save_csv():
+    pass
+
+
+
+### === App Logics ===
+def main():
+    pass
+
+### === Run App ===
+if __name__ == "__main__":
+    main()
+
+
+# notes
+# 前回より進化を目指すには？
+# カレンダー機能をつけたい。具体的には、カレンダー表示にその日の出費を表示させる＆Addできる。
+# ↑はGUI版での機能なので、CLI版では省略
+# したがって、GUI版はインターフェースをカレンダーベースにする。イメージとしては、手帳の月または週表示。
