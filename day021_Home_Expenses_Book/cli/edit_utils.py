191d87ed35765c1b4f37ac4ca9202c0253f3ddf5
Yohey-mk <youheiyamaguchi0327@gmail.com>
2025-07-20 20:19:12 +0900

    refined cli code

--- day021_Home_Expenses_Book/cli/edit_utils.py
+++ day021_Home_Expenses_Book/cli/edit_utils.py
@@ -14,14 +14,36 @@ def user_edit(df, target_df=None):
         print("Please enter a valid number.")
         return df
     else:
-        column = input("Choose which item to edit(Date/Category/Amount/Memo): ")
-        if column not in df.columns:
+        user_input = input("Choose which item to edit\n1. Date / 2. Category / 3. Amount / 4. Memo: ")
+        if user_input == "1" or user_input.lower() == "date":
+            column = "Date"
+            old_value = df.at[idx, column]
+        elif user_input == "2" or user_input.lower() == "category":
+            column = "Category"
+            old_value = df.at[idx, column]
+        elif user_input == "3" or user_input.lower() == "amount":
+            column = "Amount"
+            old_value = df.at[idx, column]
+        elif user_input == "4" or user_input.lower() == "memo":
+            column = "Memo"
+            old_value = df.at[idx, column]
+        #elif user_input not in df.columns:
+        #    print("Invalid column.")
+        #    return df
+        else:
             print("Invalid column.")
             return df
-        new_value = input(f"Enter new value for {column}: ")
-        if column == "Amount":
-            new_value = float(new_value)
+        if column in ["Date", "Category", "Memo"]:
+            new_value = input(f"Enter new value for {column}: ")
+        elif column == "Amount":
+            try:
+                new_amount = input("Enter a new value: ")
+                new_value = float(new_amount)
+            except ValueError:
+                print("Please enter valid numbers.")
+                return df
         df.at[idx, column] = new_value
+    print(f"Old value: {old_value}")
     print(f"{column} has been updated to {new_value}")
     return df
 
@@ -41,6 +63,6 @@ def edit_list(df):
             print("No entries found.")
             return df
         print(filtered_df)
-        user_edit(df)
+        user_edit(df, filtered_df)
 
         return df
