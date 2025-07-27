03f3ebca7ab1f7348f7b5f4a566193e4c69e5fdf
Yohey-mk <youheiyamaguchi0327@gmail.com>
2025-07-27 19:18:44 +0900

    initial setup

---
+++ day022_Weather_Analyzer/cli/weather_analyzer_cli.py
@@ -0,0 +1,21 @@
+# weather_analyzer_cli.py
+
+### === Imports ===
+
+
+### === Helper Functions ===
+
+
+### === App Logics ===
+def main():
+    pass
+
+### === Run App ===
+if __name__ == "__main__":
+    main()
+
+
+
+
+### Notes
+# CLIの起点。メニュー表示、ファイル指定、分析の選択などを担当。
