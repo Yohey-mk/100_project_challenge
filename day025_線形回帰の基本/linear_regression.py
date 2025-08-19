1157fb4bc52d80b3d6b7f8b02804cfc8dba9e9be
Yohey-mk <youheiyamaguchi0327@gmail.com>
2025-08-20 06:48:15 +0900

    finished coding for cli version of linear regression

--- day025_線形回帰の基本/linear_regression.py
+++ day025_線形回帰の基本/linear_regression.py
@@ -1,2 +1,42 @@
 # linear_regression.py
 
+### === Imports ===
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import sys
+import os
+
+### === Functions ===
+def resource_path(filename):
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys.MEIPASS, filename)
+    return os.path.join(os.path.abspath("."), filename)
+
+def data_loader(filename="study_score_data.csv"):
+    path = resource_path(filename)
+    df = pd.read_csv(path, encoding="utf-8")
+    return df
+
+study_data = data_loader()
+X = np.array(study_data['study_hours']).reshape(-1, 1) # 2D化
+y = np.array(study_data['score'])
+
+#モデルの作成と学習
+model = LinearRegression()
+model.fit(X, y)
+
+#予測
+X_new = np.array([[9], [1], [14], [12]])
+y_pred = model.predict(X_new)
+
+print("Prediction: ", y_pred)
+
+# Visualize
+X_sorted = np.sort(X, axis=0)
+plt.scatter(X, y, color="blue", label="Training data")
+plt.plot(X, model.predict(X_sorted), color="red", label="Regression line")
+plt.scatter(X_new, y_pred, color="green", label="Prediction (x=9)")
+plt.legend()
+plt.show
